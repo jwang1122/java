86f8ce95437e99054a2c35c42896cbcc49ff5860
John Wang <wangqianjiang@live.com>
2020-06-23 08:39:03 -0500

    add more sample code

---
+++ huaxia/src/com/huaxia/test/ExceptionTest.java
@@ -0,0 +1,31 @@
+package com.huaxia.test;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+		ExceptionTest test = new ExceptionTest();
+//		test.uncheckedArrayUsage();
+		test.tryCatchTest();
+		System.out.println(test.area(1.0f));
+		System.out.println(test.area(-2.0f));
+		System.out.println("Done.");
+	}
+
+	private void tryCatchTest() {
+		try {
+			uncheckedArrayUsage();
+		}catch(Exception e) {
+			System.out.println("Error: " + e);
+		}
+		
+	}
+	private float area(float radius) {
+		return (float) (Math.PI * radius * radius);
+	}
+	private void uncheckedArrayUsage() {
+		int[] numbers = {1,2,3,4,5};
+		System.out.println(numbers[5]);
+		
+	}
+
+}
---
+++ huaxia/src/com/huaxia/test/LoggingTest.java
@@ -0,0 +1,16 @@
+package com.huaxia.test;
+
+public class LoggingTest {
+
+	public static void main(String[] args) {
+		LoggingTest test = new LoggingTest();
+		test.logErrorTest();
+
+	}
+
+	private void logErrorTest() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
