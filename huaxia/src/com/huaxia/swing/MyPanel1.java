12865ad67fcf9e4192da99619fd52ba732299197
wangqianjiang <wangqianjiang@192.168.1.3>
2020-09-22 14:01:25 -0500

    add move image sample code

---
+++ huaxia/src/com/huaxia/swing/MyPanel1.java
@@ -0,0 +1,49 @@
+package com.huaxia.swing;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class MyPanel1 extends JPanel implements ActionListener {
+	private static final long serialVersionUID = 1L;
+	private Image apple;
+	private int apple_x = 100;
+	private int apple_y = 100;
+	private final int DELAY = 40;
+	private Timer timer;
+	
+	public MyPanel1() {
+		loadImage();
+		timer = new Timer(DELAY, this);
+		timer.start();
+	}
+	public void paint(Graphics g) {
+
+		g.drawImage(apple, apple_x, apple_y, this);
+
+	}
+	
+	private void loadImage() {
+		Toolkit t = Toolkit.getDefaultToolkit();
+		URL iconUrl = this.getClass().getResource("resources/apple.png");
+		apple = t.getImage(iconUrl);		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		move();
+		repaint();
+	}
+	
+	private void move() {
+		apple_x += 1;
+		apple_y += 1;		
+	}
+}
