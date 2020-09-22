ebff7adfb757d33cafd8e2835d9dbfe188bd05d5
wangqianjiang <wangqianjiang@192.168.1.3>
2020-09-22 16:14:01 -0500

    add calculator sample code

--- huaxia/src/com/huaxia/swing/OpenWindow.java
+++ huaxia/src/com/huaxia/swing/OpenWindow.java
@@ -6,7 +6,7 @@ public class OpenWindow {
 	public static void main(String[] args) {
 		JFrame f = new JFrame("My First Window");// creating instance of JFrame
 		f.setSize(400, 500);// 400 width and 500 height
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // terminate app on close window
 		f.setVisible(true);// making the frame visible
 	}
 }
