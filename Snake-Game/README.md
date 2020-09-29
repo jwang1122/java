805af4dcbdf949bb6740b01cb105f23de70e3f28
wangqianjiang <wangqianjiang@192.168.1.8>
2020-09-29 15:53:59 -0500

    add more sample code

--- Snake-Game/README.md
+++ Snake-Game/README.md
@@ -7,3 +7,17 @@ Java Snake eat Apple game source code
 * Use left, right, up and down arrow key to control the snake
 * Try to eat the apple without touch the window boarder.
 * The snake start with 3 dots, and gradually grows to increase the hardness of the game.
+
+## build jar file
+
+* open DOS command
+* cd to the bin folder
+* type in command
+
+```
+jar cmvf META-INF/MANIFEST.MF snake.jar .
+```
+* create shortcut of the snake.jar file
+* change the icon by using
+
+/Users/wangqianjiang/workspace/java/Snake-Game/snake.ico
---
+++ john/src/com/huaxia/john/Math1.java
@@ -0,0 +1,53 @@
+package com.huaxia.john;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class Math1 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	public Math1(String title) {
+		super(title);
+		setSize(400, 500);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+		setLayout(null);
+		setLocationRelativeTo(null);
+		
+		JTextField t1 = new JTextField();
+		t1.setBounds(10, 50, 30, 30);
+		add(t1);
+		JLabel plus = new JLabel(" + ");
+		plus.setBounds(50, 50, 30, 30);
+		add(plus);
+		JTextField t2 = new JTextField();
+		t2.setBounds(80, 50, 30, 30);
+		add(t2);
+		JTextField result = new JTextField();
+		result.setBounds(180, 50, 100, 30);
+		add(result);
+		JButton b = new JButton(" = ");// creating instance of JButton
+		b.setBounds(120, 50, 50, 30);// x axis, y axis, width, height
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int x = Integer.parseInt(t1.getText());
+				int y = Integer.parseInt(t2.getText());
+				int z = x + y;
+				result.setText( "" + z);				
+			}
+		});
+		add(b);// adding button in JFrame
+		
+//		setVisible(true);
+	}
+	public static void main(String[] args) {
+		Math1 m = new Math1("Simple Math");
+		m.setVisible(true);
+	}
+
+}
