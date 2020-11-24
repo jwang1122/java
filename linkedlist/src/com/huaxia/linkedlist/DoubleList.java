9c077143b6deaa9c48e4922f0b1f3cedf0f3f051
jwang1122 <jwang1122@gmail.com>
2020-11-23 21:01:56 -0600

    optimize DoubleList.java

--- linkedlist/src/com/huaxia/linkedlist/DoubleList.java
+++ linkedlist/src/com/huaxia/linkedlist/DoubleList.java
@@ -21,30 +21,30 @@ public class DoubleList {
 			adjustLinkedList(newNode);
 		}
 	}
-	
+
 	private void setFirstNode(DoubleNode firstNode) {
 		front = firstNode;
 		back = firstNode;
 	}
-	
+
 	private boolean findSameWord(String word) {
 		boolean found = false;
-		DoubleNode temp = front;
+		DoubleNode cursor = front;
 		do {
-			if (temp.getWord().equals(word)) {
-				temp.setWordCount(temp.getWordCount() + 1);
+			if (cursor.getWord().equals(word)) {
+				cursor.setWordCount(cursor.getWordCount() + 1);
 				found = true;
 				break;
 			}
-			temp = temp.getNext();
-		} while (temp != null);
+			cursor = cursor.getNext();
+		} while (cursor != null);
 		return found;
 	}
-	
+
 	private void adjustLinkedList(DoubleNode newNode) {
 		newNode.setPrev(back);
 		back.setNext(newNode);
-		back = newNode;		
+		back = newNode;
 	}
 
 	// REMOVE ONE COUNT OF THE WORD FROM THE LIST, IF THE COUNT RESULTS IN ZERO
@@ -85,11 +85,38 @@ public class DoubleList {
 	// IT WILL COUNT USING PREVIOUS
 	// IF X IS POSITIVE IT WILL COUNT USING NEXT
 	public String getWord(int x) {
+		if (x < 0) {
+			return traverseBackward(x);
+		}
+		return traverseForward(x);
+	}
+
+	private String traverseForward(int x) {
+		int index = 0;
+		DoubleNode cursor = front;
+		while (cursor != null) {
+			if (index++ == x) {
+				return cursor.getWord();
+			}
+			cursor = cursor.getNext();
+		}
+		return "";
+	}
+
+	private String traverseBackward(int x) {
+		int index = 0;
+		DoubleNode cursor = back;
+		while (cursor != null) {
+			if (index-- == x) {
+				return cursor.getWord();
+			}
+			cursor = cursor.getPrev();
+		}
 		return "";
 	}
 
 	// OPTIONAL PRIVATE METHOD
-	private void removeNode(DoubleNode x, DoubleNode y, DoubleNode z) {
+	void removeNode(DoubleNode x, DoubleNode y, DoubleNode z) {
 
 	}
 
@@ -106,16 +133,14 @@ public class DoubleList {
 
 	// RETURNS IF THE LIST CONTAINS THE WORD IN IT //
 	public boolean contains(String word) {
-		boolean contains = false;
 		DoubleNode temp = front;
 		while (temp != null) {
 			if (temp.getWord().equals(word)) {
-				contains = true;
-				break;
+				return true;
 			}
 			temp = temp.getNext();
 		}
-		return contains;
+		return false;
 	}
 
 	public void printNodes() {
